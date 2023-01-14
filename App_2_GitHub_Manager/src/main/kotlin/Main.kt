6dc7448a9b178ffc6d0d5cf69425a4678dd78025
Loic DUMAY <dumay.famille@gmail.com>
2023-01-14 02:39:47 +0100

    Update Main.kt

--- App_2_GitHub_Manager/src/main/kotlin/Main.kt
+++ App_2_GitHub_Manager/src/main/kotlin/Main.kt
@@ -3,13 +3,30 @@ import io.github.cdimascio.dotenv.dotenv
 
 var dotenv: Dotenv? = null
 
-fun main() {
-    println("Hello World!")
+var TOKEN_GITHUB_PUBLIC_REPO: String? = null
 
+fun main() {
+    println("GitHub Manager")
+
+    // Load .env file
     loadDotenvConfiguration()
 
-    var foo = dotenv?.get("FOO")
-    println("FOO = ${foo}")
+    /**
+     * Créé un système de connexion et de lecture des dépôts personnels github.
+     * Sources :
+     * - https://docs.github.com/fr/rest/quickstart
+     * - https://docs.github.com/fr/rest/quickstart?apiVersion=2022-11-28
+     * - https://docs.github.com/fr/rest/repos/repos?apiVersion=2022-11-28
+     */
+
+    //Récupération de la variable d'environnement TOKEN_GITHUB_PUBLIC_REPO
+    TOKEN_GITHUB_PUBLIC_REPO = dotenv?.get("GITHUB_PUBLIC_REPO")
+    //Vérification de la présence du token Github dans le fichier .env
+    if(TOKEN_GITHUB_PUBLIC_REPO == null || TOKEN_GITHUB_PUBLIC_REPO == "" || TOKEN_GITHUB_PUBLIC_REPO == "token") {
+        println("Le token GitHub \"GITHUB_PUBLIC_REPO\" est incorrect ou n'existe pas.")
+    } else {
+        println("Le token GitHub \"GITHUB_PUBLIC_REPO\" est présent.")
+    }
 }
 
 /**
